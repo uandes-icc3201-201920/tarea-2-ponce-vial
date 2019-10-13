a3f39c3688316fff1bcd5502ba40bd3ea6782e4c
Andres Vial <48449621+andresvial@users.noreply.github.com>
2019-10-13 14:19:38 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -15,10 +15,50 @@ how to use the page table and disk interfaces.
 #include <string.h>
 #include <errno.h>
 
+struct disk* disk = NULL;
+char* algoritmo;
+int nframes;
+int npages;
+char* virtmem = NULL;
+char* physmem = NULL;
+
+void algoritmo_rand(struct page_table *pt, int page);
+
+typedef struct{
+	int page;
+	int bits;	
+
+}entrada_tabla_marcos;
+
+//Creo una lista de las entradas de la tabla de marcos
+entrada_tabla_marcos* tabla_marcos;
+
 void page_fault_handler( struct page_table *pt, int page )
 {
+	//RAND
+	if(!strcmp("rand",algoritmo))
+	{
+		algoritmo_rand(pt, page);
+	}
+
+	//FIFO
+	else if(!strcmp("fifo",algoritmo))
+	{
+		//fifo_fault_handler(pt, page);
+	}
+
+	
+	/*int* frame;
+	int* bits;
+	printf("tabla de pagina:\n");
+	for (int i=0 ; i<10; i++){
+		page_table_get_entry(pt,i, &frame, &bits);
+		printf("frame=%d bits=%d\n",frame,bits);
+	}
+	printf("--------------\n");
 	printf("page fault on page #%d\n",page);
-	exit(1);
+	page_table_set_entry(pt,page,page,PROT_WRITE);
+	//exit(1);*/
 }
 
 int main( int argc, char *argv[] )
@@ -28,11 +68,12 @@ int main( int argc, char *argv[] )
 		return 1;
 	}
 
-	int npages = atoi(argv[1]);
-	int nframes = atoi(argv[2]);
+	npages = atoi(argv[1]);
+	nframes = atoi(argv[2]);
+	algoritmo = argv[3];
 	const char *program = argv[4];
 
-	struct disk *disk = disk_open("myvirtualdisk",npages);
+	disk = disk_open("myvirtualdisk",npages);
 	if(!disk) {
 		fprintf(stderr,"couldn't create virtual disk: %s\n",strerror(errno));
 		return 1;
@@ -45,9 +86,12 @@ int main( int argc, char *argv[] )
 		return 1;
 	}
 
-	char *virtmem = page_table_get_virtmem(pt);
+	//Le asigno el tamaño a la tabla de marcos
+	tabla_marcos= malloc(nframes * sizeof(entrada_tabla_marcos));
 
-	char *physmem = page_table_get_physmem(pt);
+	virtmem = page_table_get_virtmem(pt);
+
+	physmem = page_table_get_physmem(pt);
 
 	if(!strcmp(program,"pattern1")) {
 		access_pattern1(virtmem,npages*PAGE_SIZE);
@@ -65,6 +109,67 @@ int main( int argc, char *argv[] )
 
 	page_table_delete(pt);
 	disk_close(disk);
+	free(tabla_marcos);
 
 	return 0;
+}
+
+//Funcion para buscar marco vacio en la tabla de marcos, sino hay retorna -1
+int buscar_marco_vacio(){
+	int marco_vacio;
+	marco_vacio=-1;
+	for(int i= 0; i<nframes; i++)
+	{
+		if(tabla_marcos[i].bits == 0){
+			marco_vacio=i;
+		}
+	}
+	return marco_vacio;
+}
+
+void eliminar_pagina(struct page_table *pt, int marco)
+{
+	//set the entry to not be writen; update the frame table to represent that and update the front "pointer"
+	page_table_set_entry(pt, tabla_marcos[marco].page, marco, 0);
+	tabla_marcos[marco].bits = 0;
+	//front=(front+1)%nframes;
+
+}
+
+void algoritmo_rand(struct page_table *pt, int page){
+
+	printf("page fault on page #%d\n",page);
+	int frame;
+	int bits;
+	page_table_get_entry(pt, page, &frame, &bits);
+
+	printf("frame=%d bits=%d\n",frame,bits);
+
+	//Falta de pagina por no tener marco disponible (bits es 0)
+	if (bits==0){
+
+		//Primero se busca un marco vacio
+		int marco_vacio=buscar_marco_vacio();
+		
+		//Si es -1, entonces no hay marco vacio y hay que reemplazar
+		if(marco_vacio ==-1){
+
+			//como es RAND, eligir un marco victima al azar y remuevo la pagina asociada a ella en la tabla de paginas
+			int marco_victima = lrand48() % nframes;
+
+			//si la pagina del marco victima tiene bit de escritura, entonces hay que guardarlo a disco antes de borrarlo
+			if(tabla_marcos[marco_victima].bits & PROT_WRITE)
+			{
+				disk_write(disk, tabla_marcos[marco_victima].page, &physmem[marco_victima *PAGE_SIZE]);
+			}
+
+			eliminar_pagina(pt, marco_victima);
+		}
+		
+		
+	}
+
+	
+
+	 
 }
--- program.c
+++ program.c
@@ -25,6 +25,9 @@ static int compare_bytes( const void *pa, const void *pb )
 
 void access_pattern1( char *data, int length )
 {
+	for (int i = 0; i < length; i++) {	
+		data[i]=0;
+	}
 	// TODO: Implementar
 }
 
